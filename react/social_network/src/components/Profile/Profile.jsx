d47fb6e4b925bb8ba271fcdb9c4c132bd8cc3670
theV1rTual <RoadToGoooogle@gmail.com>
2021-08-05 22:29:14 +0600

    working with css modules

--- react/social_network/src/components/Profile/Profile.jsx
+++ react/social_network/src/components/Profile/Profile.jsx
@@ -3,9 +3,9 @@ import classes from './Profile.module.css'
 
 const Profile = () => {
     return (
-    <div className={classes.content}>
+    <div>
         <div>
-            <img src="https://img.freepik.com/free-vector/abstract-black-splat-grunge-background_1035-19430.jpg?size=626&ext=jpg&ga=GA1.2.2045703221.1627862400" alt="logo" />
+            <img className={classes.img} src="https://img.freepik.com/free-vector/abstract-black-splat-grunge-background_1035-19430.jpg?size=626&ext=jpg&ga=GA1.2.2045703221.1627862400" alt="logo" />
         </div>
         <div>
             ava + description
