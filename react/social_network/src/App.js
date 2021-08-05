707ce8cf75675aab54dacc4dab0500e31398c981
theV1rTual <RoadToGoooogle@gmail.com>
2021-08-05 22:49:29 +0600

    adding routers

--- react/social_network/src/App.js
+++ react/social_network/src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
 import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
@@ -6,14 +7,16 @@ import Profile from './components/Profile/Profile';
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <div className="app-wrapper-content">
-        <Dialogs />
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/profile" component={Profile} />
+        </div>
       </div>
-      {/* <Profile /> */}
-    </div>
+    </BrowserRouter>
   );
 }
 
--- react/social_network/src/components/Navbar/Navbar.jsx
+++ react/social_network/src/components/Navbar/Navbar.jsx
@@ -4,10 +4,10 @@ const Navbar = () => {
     return (
     <nav className={classes.nav}>
         <div className={classes.item}>
-            <a>Profile</a>
+            <a href="/profile">Profile</a>
         </div>
         <div className={`${classes.item} ${classes.active}`}>
-            <a>Messages</a>
+            <a href="/dialogs">Messages</a>
         </div>
         <div className={classes.item}>
             <a>News</a>
