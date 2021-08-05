42efa5edb223bdc61f6988520c18ab0e2a443bae
theV1rTual <RoadToGoooogle@gmail.com>
2021-08-05 23:36:38 +0600

    making dialogs page

--- react/social_network/src/components/Dialogs/Dialogs.jsx
+++ react/social_network/src/components/Dialogs/Dialogs.jsx
@@ -2,8 +2,33 @@ import classes from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Arystan
+                </div>
+                <div className={classes.dialog}>
+                    Diana
+                </div>
+                <div className={classes.dialog}>
+                    Dias
+                </div>
+                <div className={classes.dialog}>
+                    Aisulu 
+                </div>
+                <div className={classes.dialog}>
+                    Danel
+                </div>
+                <div className={classes.dialog}>
+                    Abl
+                </div>
+            </div>
+
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi, my friend</div>
+                <div className={classes.message}>Go footbal mb?</div>
+                <div className={classes.message}>Bye</div>
+            </div>
         </div>
     ) 
 }
