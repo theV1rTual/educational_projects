ce7b363c52339402a87cb06190c9132e13959f6e
theV1rTual <RoadToGoooogle@gmail.com>
2021-08-05 23:15:24 +0600

    adding navlink tags

--- react/social_network/src/components/Navbar/Navbar.jsx
+++ react/social_network/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.css'
 
 const Navbar = () => {
     return (
     <nav className={classes.nav}>
         <div className={classes.item}>
-            <a href="/profile">Profile</a>
+            <NavLink to="/profile" activeClassName={classes.activeLink}>Profile</NavLink>
         </div>
         <div className={`${classes.item} ${classes.active}`}>
-            <a href="/dialogs">Messages</a>
+            <NavLink to="/dialogs" activeClassName={classes.activeLink}>Messages</NavLink>
         </div>
         <div className={classes.item}>
             <a>News</a>
