ed7a81c40df7e8adff53d4db7af6e8540a2702cd
theV1rTual <RoadToGoooogle@gmail.com>
2021-08-06 10:58:07 +0600

    mapping data

--- react/social_network/src/components/Dialogs/Dialogs.jsx
+++ react/social_network/src/components/Dialogs/Dialogs.jsx
@@ -11,14 +11,12 @@ const DialogItem = (props) => {
 }
 
 const Message = (props) => {
-    return (
-        <div className={classes.message}>{props.message}</div>
-    );
+    return (<div className={classes.dialog}>{props.message}</div>);
 }
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Arystan'},
         {id: 2, name: 'Diana'},
         {id: 3, name: 'Dias'},
@@ -27,27 +25,23 @@ const Dialogs = (props) => {
         {id: 6, name: 'Abl'}
     ]
 
-    let messagesData = [
+    let messages = [
         {id: 1, message: "Hi, my friend"},
         {id: 2, message: "Go footbal mb?"},
         {id: 3, message: "Bye"}, 
     ]
 
+    let dialogsElements = dialogs.map( d => <DialogItem name={d.name} key={d.id} />);
+    let messagesElements = messages.map(m => <Message message={m.message} key={m.id}/>)
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogsItems}>
-                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/> 
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
-                <DialogItem name={dialogsData[3].name} id={dialogsData[3].id}/>
-                <DialogItem name={dialogsData[4].name} id={dialogsData[4].id}/>
-                <DialogItem name={dialogsData[5].name} id={dialogsData[5].id}/>
+                {dialogsElements}
             </div>
 
             <div className={classes.messages}>
-                <Message message={messagesData[0].message} id={messagesData[0].id} />
-                <Message message={messagesData[1].message} id={messagesData[1].id} />
-                <Message message={messagesData[2].message} id={messagesData[2].id} />
+                {messagesElements}
             </div>
         </div>
     ) 
--- react/social_network/src/components/Profile/MyPosts/MyPosts.jsx
+++ react/social_network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,10 +3,13 @@ import Post from './Post/Post'
 
 const MyPosts = (props) => {
 
-    let postsData = [
+    let posts= [
         {id: 1, message: "Hi, how are you?", likesCount: "1"},
         {id: 2, message: "It's my way of ninja", likesCount: "23"},
     ]
+
+    let postsElements = 
+        posts.map(p => <Post message={p.message} likesCount={p.likesCount} key={p.id}/>)
 
     return (
     <div className={classes.postsBlock}>
@@ -20,8 +23,7 @@ const MyPosts = (props) => {
             <button>Add post</button>
         </div>
         <div className={classes.posts}>
-            <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
-            <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
+            {postsElements}
         </div>
     </div>
     );
